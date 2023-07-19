2fa92719df594f7fbd19bd1b32920caf6f95d2d2
unknown <nafhanghifari@gmail.com>
2023-07-19 18:04:57 +0700

    Add api get user

---
+++ src/pages/api/users/get.js
@@ -0,0 +1,26 @@
+import clientPromise from "@/lib/mongodb";
+// import isAdmin from "../../../middleware/isAdmin";
+import { getSession } from "next-auth/react";
+
+//create new collection
+export default async function handler(req, res) {
+
+
+
+ 
+    
+  try { 
+    // await isAdmin(req, res, async () => {
+      const client = await clientPromise;
+      const db = client.db("boba");
+      const collection = db.collection("users");
+
+      //get db query
+      const result = await collection.find({}, {projection:{_id: 0, password:0}}).toArray();
+      res.status(200).send(result);
+      //create new collection
+    // });
+  } catch (error) {
+    res.json(error);
+  }
+}
