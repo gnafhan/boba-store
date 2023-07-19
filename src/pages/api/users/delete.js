cf4325366105d3d45ff02aa92ebb87b1a6f60cf7
unknown <nafhanghifari@gmail.com>
2023-07-19 18:59:00 +0700

    Add middleware api delete user

--- src/pages/api/users/delete.js
+++ src/pages/api/users/delete.js
@@ -1,18 +1,18 @@
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
-// import isAdmin from "../../../middleware/isAdmin";
+import isAdmin from "../../../../middleware/isAdmin";
 
 //create new collection
 export default async function handler(req, res) {
   try {
-    // await isAdmin(req, res, async () => {
-      if (req.method === "GET") {
+    await isAdmin(req, res, async () => {
+      if (req.method === "POST") {
         const { id } = req.body
           const client = await clientPromise
           const db = client.db('boba')
           const collection = db.collection('users')
         //delete one 
-        const result = await collection.deleteOne({"_id": ObjectId("64b7c170c008a39fe5927a4f")})
+        const result = await collection.deleteOne({"_id": ObjectId(id)})
     
         //   const result = await collection.deleteOne({})
           res.status(200).send(result)
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     } else {
       return res.status(403).json({ error: 'forbidden' });
     }
-    // }, "post")
+    }, "post")
   } catch (error) {
     res.json(error)
   }
