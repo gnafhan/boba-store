ecf199e20771c93ccf473fb65bff84a351c04d92
unknown <nafhanghifari@gmail.com>
2023-07-20 22:51:58 +0700

    Add Navbar to Admin Page

--- components/NavbarAdmin.js
+++ components/NavbarAdmin.js
@@ -1,221 +1,254 @@
-() => {
-    const bg = useColorModeValue("white", "gray.800");
-    const mobileNav = useDisclosure();
-    return (
-      <Box shadow="md">
-        <chakra.header
-          bg={bg}
-          borderColor="gray.600"
-          borderBottomWidth={1}
-          w="full"
-          px={{
-            base: 2,
-            sm: 4,
-          }}
-          py={4}
-        >
-          <Flex alignItems="center" justifyContent="space-between" mx="auto">
-            <HStack spacing={4} display="flex" alignItems="center">
-              <Box
+// import { Box, Flex, useColorModeValue, useDisclosure } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+  Grid,
+  GridItem,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Avatar,
+  HStack,
+  VStack,
+  CloseButton,
+  Heading,
+  Tabs,
+  TabList,
+  Tab,
+  Spacer,
+  InputGroup,
+  InputLeftElement,
+  Input,
+} from "@chakra-ui/react";
+import { BiHome } from "react-icons/bi";
+import { BsFillBellFill, BsFillCameraVideoFill, BsInbox } from "react-icons/bs";
+import { IoLogoCodepen } from "react-icons/io5";
+import { MdOutlineInbox, MdOutlineMenu } from "react-icons/md";
+
+const NavbarAdmin = () => {
+  const bg = useColorModeValue("white", "gray.800");
+  const mobileNav = useDisclosure();
+  return (
+    <Box shadow="md">
+      <Box
+        bg={bg}
+        borderColor="gray.600"
+        borderBottomWidth={1}
+        w="full"
+        px={{
+          base: 2,
+          sm: 4,
+        }}
+        py={4}
+      >
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <HStack spacing={4} display="flex" alignItems="center">
+            <Box
+              display={{
+                base: "inline-flex",
+                md: "none",
+              }}
+            >
+              <IconButton
                 display={{
-                  base: "inline-flex",
+                  base: "flex",
                   md: "none",
                 }}
-              >
-                <IconButton
-                  display={{
-                    base: "flex",
-                    md: "none",
-                  }}
-                  aria-label="Open menu"
-                  fontSize="20px"
-                  color="gray.800"
-                  _dark={{
-                    color: "inherit",
-                  }}
-                  variant="ghost"
-                  icon={<AiOutlineMenu />}
-                  onClick={mobileNav.onOpen}
-                />
-                <VStack
-                  pos="absolute"
-                  top={0}
-                  left={0}
-                  right={0}
-                  display={mobileNav.isOpen ? "flex" : "none"}
-                  flexDirection="column"
-                  p={2}
-                  pb={4}
-                  m={2}
-                  bg={bg}
-                  spacing={3}
-                  rounded="sm"
-                  shadow="sm"
-                >
-                  <CloseButton
-                    aria-label="Close menu"
-                    justifySelf="self-start"
-                    onClick={mobileNav.onClose}
-                  />
-                  <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
-                    Dashboard
-                  </Button>
-                  <Button
-                    w="full"
-                    variant="solid"
-                    colorScheme="brand"
-                    leftIcon={<AiOutlineInbox />}
-                  >
-                    Inbox
-                  </Button>
-                  <Button
-                    w="full"
-                    variant="ghost"
-                    leftIcon={<BsFillCameraVideoFill />}
-                  >
-                    Videos
-                  </Button>
-                </VStack>
-              </Box>
-              <chakra.a
-                href="/"
-                title="Choc Home Page"
-                display="flex"
-                alignItems="center"
-              >
-                <Logo />
-                <VisuallyHidden>Choc</VisuallyHidden>
-              </chakra.a>
-              <chakra.h1 fontSize="xl">Settings</chakra.h1>
-            </HStack>
-            <HStack spacing={3} display="flex" alignItems="center">
-              <HStack
-                spacing={3}
-                display={{
-                  base: "none",
-                  md: "inline-flex",
-                }}
-              >
-                <Button variant="ghost" leftIcon={<AiFillHome />} size="sm">
-                  Dashboard
-                </Button>
-                <Button
-                  variant="solid"
-                  colorScheme="brand"
-                  leftIcon={<AiOutlineInbox />}
-                  size="sm"
-                >
-                  Inbox
-                </Button>
-                <Button
-                  variant="ghost"
-                  leftIcon={<BsFillCameraVideoFill />}
-                  size="sm"
-                >
-                  Videos
-                </Button>
-              </HStack>
-              <chakra.a
-                p={3}
+                aria-label="Open menu"
+                fontSize="20px"
                 color="gray.800"
                 _dark={{
                   color: "inherit",
                 }}
-                rounded="sm"
-                _hover={{
-                  color: "gray.800",
-                  _dark: {
-                    color: "gray.600",
-                  },
-                }}
-              >
-                <AiFillBell />
-                <VisuallyHidden>Notifications</VisuallyHidden>
-              </chakra.a>
-  
-              <Avatar
-                size="sm"
-                name="Dan Abrahmov"
-                src="https://bit.ly/dan-abramov"
+                variant="ghost"
+                icon={<MdOutlineMenu />}
+                onClick={mobileNav.onOpen}
               />
-            </HStack>
-          </Flex>
-        </chakra.header>
-        <Flex
-          alignItems="center"
-          justifyContent="space-between"
-          mx={2}
-          borderWidth={0}
-          overflowX="auto"
-        >
-          <Tabs defaultIndex={1} borderBottomColor="transparent">
-            <TabList>
-              <Tab
-                py={4}
-                m={0}
-                _focus={{
-                  boxShadow: "none",
-                }}
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? "flex" : "none"}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg={bg}
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
               >
-                Basic
-              </Tab>
-              <Tab
-                py={4}
-                m={0}
-                _focus={{
-                  boxShadow: "none",
-                }}
-              >
-                Integrations
-              </Tab>
-              <Tab
-                py={4}
-                m={0}
-                _focus={{
-                  boxShadow: "none",
-                }}
-              >
-                Notifications
-              </Tab>
-              <Tab
-                py={4}
-                m={0}
-                _focus={{
-                  boxShadow: "none",
-                }}
-              >
-                Usage
-              </Tab>
-              <Tab
-                py={4}
-                m={0}
-                _focus={{
-                  boxShadow: "none",
-                }}
-              >
-                Billing
-              </Tab>{" "}
-              <Tab isDisabled py={4} m={0}>
-                Advanced
-              </Tab>
-            </TabList>
-          </Tabs>
-          <Spacer />
-          <HStack spacing={3} alignItems="center">
-            <InputGroup
+                <CloseButton
+                  aria-label="Close menu"
+                  justifySelf="self-start"
+                  onClick={mobileNav.onClose}
+                />
+                <Button w="full" variant="ghost" leftIcon={<BiHome />}>
+                  Dashboard
+                </Button>
+                <Button
+                  w="full"
+                  variant="solid"
+                  colorScheme="brand"
+                  leftIcon={<MdOutlineInbox />}
+                >
+                  Inbox
+                </Button>
+                <Button
+                  w="full"
+                  variant="ghost"
+                  leftIcon={<BsFillCameraVideoFill />}
+                >
+                  Videos
+                </Button>
+              </VStack>
+            </Box>
+            <Box
+              href="/"
+              title="Choc Home Page"
+              display="flex"
+              alignItems="center"
+            >
+              <IoLogoCodepen />
+              <Text>Choc</Text>
+            </Box>
+          </HStack>
+          <HStack spacing={3} display="flex" alignItems="center">
+            <HStack
+              spacing={3}
               display={{
                 base: "none",
-                lg: "block",
+                md: "inline-flex",
               }}
-              ml="auto"
             >
-              <InputLeftElement pointerEvents="none">
-                <AiOutlineSearch />
-              </InputLeftElement>
-              <Input type="tel" placeholder="Search..." />
-            </InputGroup>
+              <Button variant="ghost" leftIcon={<BiHome />} size="sm">
+                Dashboard
+              </Button>
+
+              <Button
+                variant="ghost"
+                leftIcon={<BsFillCameraVideoFill />}
+                size="sm"
+              >
+                Videos
+              </Button>
+            </HStack>
+            <Link
+              p={3}
+              color="gray.800"
+              _dark={{
+                color: "inherit",
+              }}
+              rounded="sm"
+              _hover={{
+                color: "gray.800",
+                _dark: {
+                  color: "gray.600",
+                },
+              }}
+            >
+              <BsFillBellFill />
+            </Link>
+
+            <Avatar
+              size="sm"
+              name="Dan Abrahmov"
+              src="https://bit.ly/dan-abramov"
+            />
           </HStack>
         </Flex>
       </Box>
-    );
-  };
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        mx={2}
+        borderWidth={0}
+        overflowX="auto"
+      >
+        <Tabs defaultIndex={1} borderBottomColor="transparent">
+          <TabList>
+            <Tab
+              py={4}
+              m={0}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              Basic
+            </Tab>
+            <Tab
+              py={4}
+              m={0}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              Integrations
+            </Tab>
+            <Tab
+              py={4}
+              m={0}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              Notifications
+            </Tab>
+            <Tab
+              py={4}
+              m={0}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              Usage
+            </Tab>
+            <Tab
+              py={4}
+              m={0}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              Billing
+            </Tab>{" "}
+          </TabList>
+        </Tabs>
+        <Spacer />
+        <HStack spacing={3} alignItems="center">
+          <InputGroup
+            display={{
+              base: "none",
+              lg: "block",
+            }}
+            ml="auto"
+          >
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon />
+            </InputLeftElement>
+            <Input type="tel" placeholder="Search..." />
+          </InputGroup>
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default NavbarAdmin;
--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -29,36 +29,36 @@ import {
 import { useRouter } from "next/router";
 import axios from "axios";
 import { AddIcon, CloseIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import AdminAuth from './../../../utils/AdminAuth';
+import AdminAuth from "./../../../utils/AdminAuth";
 import { getSession, useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
+import NavbarAdmin from "../../../components/NavbarAdmin";
 
 export async function getServerSideProps() {
   try {
     // Mengambil data dari API menggunakan axios atau metode lainnya
-    const response = await axios.get("http://localhost:3000/api/get"
+    const response = await axios.get(
+      "http://localhost:3000/api/get",
 
-    //add current session
-    , {
-      headers: {
-        Authorization: process.env.BEARER_AUTH,
-      
-      },
-      withCredentials:true
-
-    }
-
+      //add current session
+      {
+        headers: {
+          Authorization: process.env.BEARER_AUTH,
+        },
+        withCredentials: true,
+      }
     );
 
     // Mendapatkan data dari response
     const data = response.data;
-    const bearer = process.env.BEARER_AUTH;''
+    const bearer = process.env.BEARER_AUTH;
+    ("");
 
     // Mengembalikan data sebagai props
     return {
       props: {
         data,
-        bearer
+        bearer,
       },
     };
   } catch (error) {
@@ -73,13 +73,12 @@ export async function getServerSideProps() {
   }
 }
 
-const  AdminIndex= ({ data, bearer })=> {
+const AdminIndex = ({ data, bearer }) => {
   const toast = useToast();
   const router = useRouter();
   const [showToast, setShowToast] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedData, setSelectedData] = useState(null);
-
 
   useEffect(() => {
     const { create, deleted, edit } = router.query;
@@ -128,9 +127,13 @@ const  AdminIndex= ({ data, bearer })=> {
 
   const handleDelete = async () => {
     try {
-      const response = await axios.post(`http://localhost:3000/api/delete`, {
-        id: selectedData._id,
-      }, {headers:{Authorization:bearer}});
+      const response = await axios.post(
+        `http://localhost:3000/api/delete`,
+        {
+          id: selectedData._id,
+        },
+        { headers: { Authorization: bearer } }
+      );
       onClose();
       router.push({ pathname: "/admin", query: { deleted: "success" } });
       // Lakukan apa yang perlu dilakukan setelah sukses mengirim data
@@ -167,65 +170,67 @@ const  AdminIndex= ({ data, bearer })=> {
   };
 
   return (
-    <Box ml="5%">
-      <Box mt={10}>
-        <Button colorScheme="teal" onClick={handleCreate}>
-          <AddIcon mr={3} />
-          Create
-        </Button>
+    <Box>
+      <NavbarAdmin/>
+      <Box ml="5%">
+        <Box mt={10}>
+          <Button colorScheme="teal" onClick={handleCreate}>
+            <AddIcon mr={3} />
+            Create
+          </Button>
+        </Box>
+        <Box display="flex" flexWrap="wrap" justifyContent="flex-start" p={10}>
+          {data.map((item) => {
+            return (
+              <Card key={item._id} maxW="sm" m={5}>
+                <CardBody>
+                  <div>{renderImage(item.image)}</div>
+                  <Stack mt="6" spacing="3">
+                    <Heading size="md">{item.name}</Heading>
+                    <Text>{item.description}</Text>
+                    <Text color="blue.600" fontSize="2xl">
+                      {item.price}
+                    </Text>
+                  </Stack>
+                </CardBody>
+                {/* <Divider /> */}
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <Button
+                      variant="solid"
+                      colorScheme="teal"
+                      onClick={() => handleAction(item)}
+                    >
+                      Action
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </Box>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{selectedData && selectedData.name}</ModalHeader>
+            <ModalBody>{selectedData && selectedData._id}</ModalBody>
+            <ModalFooter>
+              <Button colorScheme="yellow" mr="auto" onClick={handleEdit}>
+                <EditIcon mr={2} /> Edit
+              </Button>
+              <Button colorScheme="red" mr="auto" onClick={handleDelete}>
+                <DeleteIcon mr={2} />
+                Delete
+              </Button>
+              <Button colorScheme="blue" ml="auto" onClick={onClose}>
+                <CloseIcon></CloseIcon> Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
-      <Box display="flex" flexWrap="wrap" justifyContent="flex-start" p={10}>
-        {data.map((item) => {
-          return (
-            <Card key={item._id} maxW="sm" m={5}>
-              <CardBody>
-                <div >{renderImage(item.image)}</div>
-                <Stack mt="6" spacing="3">
-                  <Heading size="md">{item.name}</Heading>
-                  <Text>{item.description}</Text>
-                  <Text color="blue.600" fontSize="2xl">
-                    {item.price}
-                  </Text>
-                </Stack>
-              </CardBody>
-              {/* <Divider /> */}
-              <CardFooter>
-                <ButtonGroup spacing="2">
-                  <Button
-                    variant="solid"
-                    colorScheme="teal"
-                    onClick={() => handleAction(item)}
-                  >
-                    Action
-                  </Button>
-                </ButtonGroup>
-              </CardFooter>
-            </Card>
-          );
-        })}
-      </Box>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{selectedData && selectedData.name}</ModalHeader>
-          <ModalBody>{selectedData && selectedData._id}</ModalBody>
-          <ModalFooter>
-            <Button colorScheme="yellow" mr="auto" onClick={handleEdit}>
-              <EditIcon mr={2} /> Edit
-            </Button>
-            <Button colorScheme="red" mr="auto" onClick={handleDelete}>
-              <DeleteIcon mr={2} />
-              Delete
-            </Button>
-            <Button colorScheme="blue" ml="auto" onClick={onClose}>
-              <CloseIcon></CloseIcon> Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
     </Box>
   );
-}
-
+};
 
 export default AdminAuth(AdminIndex);
