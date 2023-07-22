056d285374d4e4be7ea8a509608fa323849e2c27
unknown <nafhanghifari@gmail.com>
2023-07-22 09:39:09 +0700

    Add profile picture to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -48,7 +48,7 @@ import { MdShoppingCartCheckout } from 'react-icons/md';
 
   export default function WithSubnavigation() {
     const { isOpen, onToggle } = useDisclosure();
-    const {status} = useSession();
+    const {status,data} = useSession();
   
     return (
       <Box>
@@ -106,7 +106,7 @@ import { MdShoppingCartCheckout } from 'react-icons/md';
                 <Avatar
                   size={'sm'}
                   src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    data.user.image
                   }
                 />
               </MenuButton>
