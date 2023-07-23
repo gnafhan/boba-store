56417f3209be367058ca88ab46501e9502ca8eab
unknown <nafhanghifari@gmail.com>
2023-07-23 14:50:26 +0700

    add api edit role

---
+++ src/pages/api/users/edit.js
@@ -0,0 +1,30 @@
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+import isAdmin from "../../../../middleware/isAdmin";
+
+
+export default async function handler(req, res) {
+  try {
+    await isAdmin(req, res, async () => {
+      if (req.method === "GET") {
+        const { role, email } = req.body;
+        const client = await clientPromise;
+        const db = client.db("boba");
+        const collection = db.collection("users");
+        const result = await collection.updateOne(
+          { email: email},
+          {
+            $set: {
+                role: role,
+            },
+          }
+        );
+        res.status(200).send(result);
+      } else {
+        return res.status(403).json({ error: "forbidden" });
+      }
+    }, "post");
+  } catch (error) {
+    res.json(error);
+  }
+}
