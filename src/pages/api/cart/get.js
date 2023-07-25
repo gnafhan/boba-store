96cb24f4d90573acb86955b20b6a2105d7acad16
unknown <nafhanghifari@gmail.com>
2023-07-25 18:20:13 +0700

    add get cart api with dummy

---
+++ src/pages/api/cart/get.js
@@ -0,0 +1,29 @@
+import clientPromise from "@/lib/mongodb";
+import { getSession } from "next-auth/react";
+import isAdmin from "../../../../middleware/isAdmin";
+
+//create new collection
+export default async function handler(req, res) {
+
+
+
+ 
+    
+  try { 
+    // await isAdmin(req, res, async () => {
+        const email = "user@example.com";
+
+        // const { email } = req.body;
+      const client = await clientPromise;
+      const db = client.db("boba");
+      const collection = db.collection("cart");
+
+      //get db query
+      const result = await collection.findOne({email})
+      res.status(200).send(result);
+      //create new collection
+    // });
+  } catch (error) {
+    res.json(error);
+  }
+}
